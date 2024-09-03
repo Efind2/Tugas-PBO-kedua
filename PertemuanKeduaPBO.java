7199e4f221d9a9def41e642cadd7c3932334e12d
Achmad Afifudin <152256295+Efind2@users.noreply.github.com>
2024-09-03 15:05:58 +0700

    Update PertemuanKeduaPBO.java

--- PertemuanKeduaPBO.java
+++ PertemuanKeduaPBO.java
@@ -3,28 +3,19 @@ package pertemuankeduapbo;
 public class PertemuanKeduaPBO {
 
     public static void main(String[] args) {
-        Adik Rara = new Adik();
-
-        System.out.println("Nama saya : " + Rara.getNama() + " |Umur : " + Rara.getUsia()+  " Sedang "+ Rara.getBermain());
-        System.out.println(Rara.getMemberiUangSaku());
         System.out.println("");
-        Ayah Kohan = new Ayah();
-        System.out.println("Nama saya : " + Kohan.getNama() + " |Umur : " + Kohan.getUsia() + " |Pekerjaan :" + Kohan.getPekerjaan() + " |Pendidikan :" + Kohan.getPendidikan() +  " dan memberi uang saku anak " + Kohan.getMemberiUangSaku());
+        Anak gaga = new Anak();
+        gaga.belajar();
+        gaga.mengajar();
+        gaga.menulis();
+        gaga.ujian();
+        gaga.getBermain();
         
-        System.out.println("");
-        Kakek Bohan = (Kakek) Kohan;
-        System.out.println("Nama saya : " + Bohan.getNama() + " |Umur : " + Bohan.getUsia() + " |Aset :" + Bohan.getAset());
-
-        System.out.println("");
-        Aku Gaga = new Aku();
-        Gaga.belajar();
-        Gaga.mengajar();
-        Gaga.menulis();
-        Gaga.ujian();
         int uang = 20000;
-        System.out.println(uang);
+        System.out.println("Aku memiliki uang "+uang);
         double uangku = (double) uang;
-        System.out.println("aku memiliki uang sejumlah "+uangku);
+        System.out.println("aku memiliki uang sejumlah " + uangku);
+        
     }
 
 }
